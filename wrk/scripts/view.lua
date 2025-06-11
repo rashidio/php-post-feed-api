ce2c7b942639f108e9ac01f6516661a0bb727c75
Rashid Suleimanov <rsuleimanov@bdainc.com>
2025-06-11 17:22:57 +0200

    POST requests in view endpoint lua performance test

--- wrk/scripts/view.lua
+++ wrk/scripts/view.lua
@@ -4,6 +4,8 @@ math.randomseed(os.time())
 request = function()
   local user_id = math.random(1, 100)
   local post_id = math.random(1, 50000)
-  local path = string.format("/api/view.php?user_id=%d&post_id=%d", user_id, post_id)
-  return wrk.format("GET", path)
+  local body = string.format("user_id=%d&post_id=%d", user_id, post_id)
+  local headers = {}
+  headers["Content-Type"] = "application/x-www-form-urlencoded"
+  return wrk.format("POST", "/api/view.php", headers, body)
 end
